ef95bf531a3ff252a84f319808abc612e1073c54
Jared <jaredjef4son@gmail.com>
2024-04-21 19:22:54 -0400

    deleted ExerciseFolder

--- ExerciseFolder/workout-app/src/componets/DurationExercise.js
+++
@@ -1,9 +0,0 @@
-import Stopwatch from "./Stopwatch"
-export default function DurationExercise ({exercise, setMenuScreen}) {
-    let {name} = exercise
-    return <div>
-      <p>{name}</p>
-      <Stopwatch/>
-      <button onClick={setMenuScreen} style={{fontSize: "1.5em"}}>Back to Menu</button>
-    </div>
-  } 
